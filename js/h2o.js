2df3ba09e30640dcf077daff2c15dd72ed587367
Garett Pascual-Folster <31081158+GarettPF@users.noreply.github.com>
2020-02-23 03:11:33 -0800

    update

--- js/h2o.js
+++ js/h2o.js
@@ -1,18 +1,30 @@
 var oxygen = document.getElementById("oxygen-marker"),
-    hydrogen = document.getElementById("hydrogen-marker")
+    hydrogen = document.getElementById("hydrogen-marker"),
+    button = document.getElementById("combine");
+
+
+function activate_button() {
+    button.disabled = false;
+
+    button.addEventListener("onkeyup", (e)=> {
+        console.log("fuck my ass!");
+    });
+}
 
 oxygen.addEventListener("markerFound", (e)=> {
-    console.log("yes");
 
-    if (hydrogen.object3D.visible == true) {
-        console.log("no");
+    while (hydrogen.object3D.visible == true) {
+        activate_button();
     }
+    button.disabled = true;
 });
 
 hydrogen.addEventListener("markerFound", (e)=> {
-    console.log("yes");
-
-    if (oxygen.object3D.visible == true) {
-        console.log("no");
+    
+    while (oxygen.object3D.visible == true) {
+        activate_button();
     }
+    button.disabled = true;
 });
+
+
